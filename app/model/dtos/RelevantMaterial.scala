f3029d23b05f46c5940bc5287273c5644058898b
Pavlos Isaris <paulisaris@gmail.com>
2015-07-07 12:59:18 +0300

    Added relevant material title for each file

--- app/model/dtos/RelevantMaterial.scala
+++ app/model/dtos/RelevantMaterial.scala
@@ -4,6 +4,7 @@ package model.dtos
  * Created by pisaris on 3/7/2015.
  */
 case class RelevantMaterial (val id: Long,
+                             val title:String,
                              val consultationId:Long,
                              val source_url:String,
                              val pdf_url:String);
--- app/model/repositories/anorm/RelevantMaterialParser.scala
+++ app/model/repositories/anorm/RelevantMaterialParser.scala
@@ -9,13 +9,14 @@ object RelevantMaterialParser{
 
   val Parse: RowParser[RelevantMaterial] = {
 
-    long("id") ~
+      long("id") ~
+      str("title") ~
       long("consultation_id") ~
       str("url_source") ~
       str("actual_pdf_url") map
       {
-        case id ~ consultation_id ~ src_url ~ pdf_url =>
-          new RelevantMaterial(id, consultation_id, src_url, pdf_url)
+        case id ~ title ~ consultation_id ~ src_url ~ pdf_url =>
+          new RelevantMaterial(id, title, consultation_id, src_url, pdf_url)
       }
 
   }
