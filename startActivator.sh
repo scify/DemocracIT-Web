1d3b2d4d69557ce629474c54fb326f31f29a9f36
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2015-07-17 16:34:25 +0300

    update configuration

--- startActivator.sh
+++ startActivator.sh
@@ -6,6 +6,6 @@ cd $workdir/DemocracIT-Web
 
 . /etc/profile;
 echo "Pass is `set | grep democracit`";
-JAVA_OPTS="-Xms128m -Xmx256m" ./activator "run 80"
+JAVA_OPTS="-Xms128m -Xmx256m" ./activator "run 80" -Dconfig.file=/home/azureuser/web/DemocracIT-Web/conf/application.prod.conf
 # &>> $activator_log
-#JAVA_OPTS="-Xms128m -Xmx256m" ./activator "run 80" &>> $activator_log
+#JAVA_OPTS="-Xms128m -Xmx256m" ./activator "run 80 " &>> $activator_log
