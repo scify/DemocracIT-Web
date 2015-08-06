d187650e5275efea546692cd9bf8b4cc030a37e7
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2015-08-06 13:46:05 +0300

    seperate annotator functionality to different javascript file

--- public/js/consultation/scify-consultation-indexPage-handler.js
+++ public/js/consultation/scify-consultation-indexPage-handler.js
@@ -24,157 +24,10 @@ scify.ConsultationIndexPageHandler = function( consultationid,userId,fullName,
 };
 scify.ConsultationIndexPageHandler.prototype = function(){
 
-    var getSelection = function() {
-        if (window.getSelection)
-          return window.getSelection();
-        else if (document.selection)
-          return document.selection;
-
-            return null;
-    },
-    getSelectionText= function(selection){
-        if (window.getSelection)
-            return selection.toString();
-        else if (document.selection)
-            return selection.createRange().text;
-    },
-    getSelectionHtml = function(selection) {
-        var html = "";
-        if (window.getSelection) {
-            if (selection.rangeCount) {
-                var container = document.createElement("div");
-                for (var i = 0, len = selection.rangeCount; i < len; ++i) {
-                    container.appendChild(selection.getRangeAt(i).cloneContents());
-                }
-                html = container.innerHTML;
-            }
-        } else if (document.selection) {
-            if (selection.type == "Text")
-                html = document.selection.createRange().htmlText;
-        }
-        return html;
-    },
-    selectionIsAllowed = function(selection){
-      var html = getSelectionHtml(selection);
-        return html.trim().length>0 && html.indexOf("span")==-1;
-    },
-    clearSelection= function(selection){
-        if(selection){
-            if(selection.empty)
-                selection.empty();
-
-            if(selection.removeAllRanges)
-                selection.removeAllRanges();
-        }
-    },
-    attachAnnotationEvents = function(){
-        var instance = this;
-        $("body").on("mouseup",".ann",function(e){
-            var selection= getSelection();
-            if (!selectionIsAllowed(selection)){
-                clearSelection(selection);
-                hideToolBar();
-            }
-            else
-                displayToolBar.call(instance,e,getSelectionText(selection));
-        });
-        $("body").on("click",".ann-icon",displayToolBar.bind(instance));
-
-    },
-    expandArticleOnClick = function(){
+    var expandArticleOnClick = function(){
         var article = $(this).closest(".article");
         if (!article.find(".article-body").hasClass("in"))
             article.find(".show-hide").trigger("click");
-    },
-    recurseAllTextNodesAndApply = function(element,action){
-
-        if (element.className && element.className.indexOf("skip-ann")>=0)
-            return;
-
-        if (elementCanBeAnnotated(element))
-        {
-            action(element);
-            return; //dont allow iteration to children
-        }
-
-        if (element.childNodes.length > 0)
-        for (var i = 0; i < element.childNodes.length; i++)
-        {
-             recurseAllTextNodesAndApply(element.childNodes[i],action);
-        }
-    },
-    elementCanBeAnnotated= function(element) {
-        //an element can be annotated if its a #TEXT node with actual text
-        if (element.nodeType == Node.TEXT_NODE && element.nodeValue.trim() != '')
-            return true;
-
-        //an element can be annotated if all it's children are #TEXT OR SUP nodes
-        var bannedNodeFound=false;
-        for (var i = 0; i < element.childNodes.length; i++) {
-            if ( element.childNodes[i].nodeName !="SUP" &&
-                 element.childNodes[i].nodeName !="#text" &&
-                element.childNodes[i].nodeName !="STRONG" )
-                bannedNodeFound=true; //the child node is not SUP and is not #TEXT node
-        }
-        if (bannedNodeFound || element.textContent.trim().length==0)
-            return false;
-
-        return true;
-
-    },
-    createAnnotatableAreas = function() {
-        var counter=0;
-        var action = function(element)
-        {
-            $(element).wrap("<div data-id='ann-"+counter+"' class='ann'></div>");
-            counter++;
-        }
-        $(".article-body,.article-title-text").each(function(i,el){
-            var html = $(el).html();
-            html = html.replace(/<br>/g,"#brNode#").replace(/<br\/>/g,"#brNode#");
-            $(el).html(html);
-           recurseAllTextNodesAndApply(el,action );
-            html= $(this).html().replace(/#brNode#/g,"<br>");
-            $(this).html(html);
-        });
-
-    },
-    attachAnnotationPrompts= function(){
-        $(".ann").append("<div class='ann-icon'>κλικ εδώ για σχολιασμό (ή επιλέξτε μέρος του κειμένου)</div>");
-    },
-    displayToolBar = function(e,selectedText){
-        //todo: Use react.js for this.
-
-       var target = $(e.target),
-           toolbar = $("#toolbar"),
-           toolbarClass ="",
-           left = e.clientX,
-           top = e.clientY + 20;
-
-        if (target.hasClass("ann-icon"))
-        {
-            selectedText = target.parent().text();
-            selectedText = selectedText.substr(0,selectedText.length-target.text().length); //remove ann-icon text
-          //  left =left - toolbar.width();
-        }
-
-        $("#toolbar-modal").modal("show");
-       // toolbar.addClass(toolbarClass);
-      //  toolbar.fadeIn("fast");
-      //  toolbar.css({top:top, left: left});
-        toolbar.find("input[name='annText']").val(selectedText);
-
-        var parent = target.closest(".ann")
-        var annid=parent.data("id");
-        var articleid=target.closest(".article").data("id");
-        toolbar.find("input[name='articleid']").val(articleid);
-        toolbar.find("input[name='discussionroomannotationtagid']").val(annid);
-        toolbar.find("blockquote").text(selectedText);
-
-    },
-    hideToolBar = function(){
-        $("#toolbar-modal").modal("hide");
-            // $("#toolbar").hide();
     },
     getDiscussionThreadNumberOfComments = function(articleId,annotationId){
         if (this.discussionThreads.hasOwnProperty(getDiscussionThreadClientId(articleId,annotationId)))
@@ -245,7 +98,7 @@ scify.ConsultationIndexPageHandler.prototype = function(){
             }
         });
     },
-     handleAnnotationSave = function(e){
+    handleAnnotationSave = function(e){
          e.preventDefault();
          var form = $("#toolbar-modal").find("form");
          var data = {};
@@ -255,14 +108,14 @@ scify.ConsultationIndexPageHandler.prototype = function(){
          getDiscussionRoom(data.articleid,data.discussionroomannotationtagid).saveComment(form.attr("action"),data);
          hideToolBar();
      },
-     replaceRelevantLaws = function(relevantLaws) {
+    replaceRelevantLaws = function(relevantLaws) {
             for (var i=0; i<relevantLaws.length; i++) {
                 var replaceText = relevantLaws[i].entity_text;
                 var replacedHtml = $("div[data-id="+ relevantLaws[i].article_id +"]").html().replace(relevantLaws[i].entity_text, "<a target='_blank' href='" + relevantLaws[i].pdf_url + "'>" + replaceText + "</a>");
                 $("div[data-id="+ relevantLaws[i].article_id +"]").html(replacedHtml);
             }
         },
-        removeParagraphsWithNoText = function(){
+    removeParagraphsWithNoText = function(){
             $(".message").find("p").each(function(i,el){
                 if ($.trim($(this).text()).length==0)
                     $(this).remove();
@@ -272,34 +125,19 @@ scify.ConsultationIndexPageHandler.prototype = function(){
         var instance= this;
         moment.locale('el');
 
-        createAnnotatableAreas();
-        attachAnnotationPrompts();
-        attachAnnotationEvents();
-
         replaceRelevantLaws(this.relevantLaws);
         addRelevantLawsHandler();
         $(".article-title-text").click(expandArticleOnClick);
         $(".article-title-text").first().trigger("click");
 
-        //$("body").on("click",".open-gov-comments", fetchOpenGovComments);
         createDiscussionRooms.call(instance);
         removeParagraphsWithNoText();
         //tinymce.init({selector:'textarea'})
         // $("#toolbar").find(".close").click(hideToolBar);
         $("#save-annotation").click(handleAnnotationSave);
 
-        $("#annotationTagId").select2({
-           placeholder: "Υπόδειξη προβλήματος",
-           tags: true,
-           tokenSeparators: [',', ' ']
-        });
-
-        $("#annotationTagTopicId").select2({
-           placeholder: "Υπόδειξη θέματος",
-           tags: true,
-           tokenSeparators: [',', ' ']
-        });
-
+        var annotator = new scify.Annotator();
+        annotator.init();
 
     };
 
@@ -307,4 +145,6 @@ scify.ConsultationIndexPageHandler.prototype = function(){
         init:init
     }
 }();
+
+
 
