a0fbc0c738411a5c416454800840078530fa2111
Alex <alexandros.tzoumas@gmail.com>
2015-08-16 12:13:01 +0300

    set greek resources

--- public/js/consultation/scify-consultationSearchPageHandler.js
+++ public/js/consultation/scify-consultationSearchPageHandler.js
@@ -15,7 +15,24 @@ scify.ConsultationSearchPageHandler.prototype = function(){
                 { title: "άρθρα" },
                 //{ title: "Διάρκεια" },
                 //{ title: "Λήξη" }
-            ]
+            ],
+            language: {
+                "sProcessing":   "Επεξεργασία...",
+                "sLengthMenu":   "Δείξε _MENU_ εγγραφές",
+                "sZeroRecords":  "Δεν βρέθηκαν εγγραφές που να ταιριάζουν",
+                "sInfo":         "Δείχνοντας _START_ εως _END_ από _TOTAL_ εγγραφές",
+                "sInfoEmpty":    "Δείχνοντας 0 εως 0 από 0 εγγραφές",
+                "sInfoFiltered": "(φιλτραρισμένες από _MAX_ συνολικά εγγραφές)",
+                "sInfoPostFix":  "",
+                "sSearch":       "Αναζήτηση:",
+                "sUrl":          "",
+                "oPaginate": {
+                    "sFirst":    "Πρώτη",
+                    "sPrevious": "Προηγούμενη",
+                    "sNext":     "Επόμενη",
+                    "sLast":     "Τελευταία"
+                }
+            }
         } );
     }
 
