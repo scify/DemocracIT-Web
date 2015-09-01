0cb507481f8f51c4fdc088f384edbdaab1bdd0a3
paulisaris@gmail.com <paulisaris@gmail.com>
2015-09-01 12:29:10 +0300

    Updated configuration file to include application state, using cookies to determing whether the user has visited again to show the tutorial

--- public/js/consultation/scify-tutorialAnnotator-compiled.js
+++ public/js/consultation/scify-tutorialAnnotator-compiled.js
@@ -13,10 +13,42 @@ scify.TutorialAnnotator.prototype = (function () {
             $(this).attr("title", "κλικ εδώ για σχολιασμού όλου του άρθρου");
         });
 
+        if (checkAndReadCookie()) {
+            $("[data-id=\"ann-0\"]").trigger("mouseover");
+            startIntro();
+        }
+
         $("#tutorial").on("click", function () {
             $("[data-id=\"ann-0\"]").trigger("mouseover");
             startIntro();
         });
+    },
+        checkAndReadCookie = function checkAndReadCookie() {
+        var tutorialCookie = getCookie("tutorialCookie");
+        console.log(tutorialCookie);
+        if (tutorialCookie != "") {
+            return false;
+        } else {
+            setCookie("tutorialCookie", "tutorial", 5 * 365);
+            console.log(tutorialCookie);
+        }
+        return true;
+    },
+        getCookie = function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(";");
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == " ") c = c.substring(1);
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+        return "";
+    },
+        setCookie = function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
     },
         startIntro = function startIntro() {
         var intro = introJs();
