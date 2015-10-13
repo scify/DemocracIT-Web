7b738829f7343e86c602fa19a305cf6d39e5b8c0
paulisaris@gmail.com <paulisaris@gmail.com>
2015-10-13 12:44:35 +0300

    DW-156 fixed annotationTagsPerArticle and problemsPerArticle

--- public/js/consultation/reporter/scify-consultationReporterPageHandler.js
+++ public/js/consultation/reporter/scify-consultationReporterPageHandler.js
@@ -35,7 +35,13 @@ scify.ConsultationReporterPageHandler = function( consultationid,userId,fullName
     this.commentsPerArticle =[];
     for (var i=0; i<commentsPerArticle.length; i++)
     {
-        this.commentsPerArticle.push([commentsPerArticle[i].title.substr(0, commentsPerArticle[i].title.indexOf(':')), commentsPerArticle[i].commentsNum, '<div style="padding-left: 10px"><h5 style="width:100%">' + commentsPerArticle[i].title + '</h5>' + '<h5>Σχόλια: ' + commentsPerArticle[i].commentsNum + '</h5></div>', commentsPerArticle[i].id ])
+        var articleTitle = "";
+        if(commentsPerArticle[i].title.indexOf(':') === -1) {
+            articleTitle = commentsPerArticle[i].title;
+        } else {
+            articleTitle = commentsPerArticle[i].title.substr(0, commentsPerArticle[i].title.indexOf(':'))
+        }
+        this.commentsPerArticle.push([articleTitle, commentsPerArticle[i].commentsNum, '<div style="padding-left: 10px"><h5 style="width:100%">' + commentsPerArticle[i].title + '</h5>' + '<h5>Σχόλια: ' + commentsPerArticle[i].commentsNum + '</h5></div>', commentsPerArticle[i].id ])
     }
 
     this.annotationsForConsultation = [];
@@ -53,16 +59,32 @@ scify.ConsultationReporterPageHandler = function( consultationid,userId,fullName
     this.annotationsPerArticle = [];
     for (var i=0; i<annotationsPerArticle.length; i++)
     {
-        this.annotationsPerArticle.push([annotationsPerArticle[i].article_name.substr(0, annotationsPerArticle[i].article_name.indexOf(':')) + ": " + annotationsPerArticle[i].annotationTag.description,  annotationsPerArticle[i].numberOfComments, '<div style="padding-left: 10px"><h5 style="width:100%">' + annotationsPerArticle[i].article_name + '<div>Tag: ' + annotationsPerArticle[i].annotationTag.description + '</div></h5>' + '<h5>Σχόλια: ' + annotationsPerArticle[i].numberOfComments + '</h5></div>' ])
+        var articleTitle = "";
+        if(annotationsPerArticle[i].article_name.indexOf(':') === -1) {
+            articleTitle = annotationsPerArticle[i].article_name;
+        } else {
+            articleTitle = annotationsPerArticle[i].article_name.substr(0, annotationsPerArticle[i].article_name.indexOf(':'));
+        }
+        this.annotationsPerArticle.push([articleTitle + ": " + annotationsPerArticle[i].annotationTag.description,  annotationsPerArticle[i].numberOfComments, '<div style="padding-left: 10px"><h5 style="width:100%">' + annotationsPerArticle[i].article_name + '<div>Tag: ' + annotationsPerArticle[i].annotationTag.description + '</div></h5>' + '<h5>Σχόλια: ' + annotationsPerArticle[i].numberOfComments + '</h5></div>', annotationsPerArticle[i].article_id ])
     }
 
     this.annotationProblemsPerArticle = [];
+    console.log(annotationProblemsPerArticle);
     for (var i=0; i<annotationProblemsPerArticle.length; i++)
     {
-        this.annotationProblemsPerArticle.push([annotationProblemsPerArticle[i].article_name.substr(0, annotationProblemsPerArticle[i].article_name.indexOf(':')) + ": " + annotationProblemsPerArticle[i].annotationTag.description,  annotationProblemsPerArticle[i].numberOfComments, '<div style="padding-left: 10px"><h5 style="width:100%">' + annotationProblemsPerArticle[i].article_name + '<div>Tag: ' + annotationProblemsPerArticle[i].annotationTag.description + '</div></h5>' + '<h5>Σχόλια: ' + annotationProblemsPerArticle[i].numberOfComments + '</h5></div>' ])
+        var articleTitle = "";
+        if(annotationProblemsPerArticle[i].article_name.indexOf(':') === -1) {
+            articleTitle = annotationProblemsPerArticle[i].article_name;
+        } else {
+            articleTitle = annotationProblemsPerArticle[i].article_name.substr(0, annotationsPerArticle[i].article_name.indexOf(':'));
+        }
+        this.annotationProblemsPerArticle.push([articleTitle + ": " + annotationProblemsPerArticle[i].annotationTag.description,  annotationProblemsPerArticle[i].numberOfComments, '<div style="padding-left: 10px"><h5 style="width:100%">' + annotationProblemsPerArticle[i].article_name + '<div>Tag: ' + annotationProblemsPerArticle[i].annotationTag.description + '</div></h5>' + '<h5>Σχόλια: ' + annotationProblemsPerArticle[i].numberOfComments + '</h5></div>', annotationProblemsPerArticle[i].article_id ])
     }
 
+    console.log(this.commentsPerArticle);
+    console.log(this.annotationsPerArticle);
     console.log(this.annotationProblemsPerArticle);
+
     getUserById = function(userId) {
         for(var i=0; i<commenters.length; i++) {
             if(userId == commenters[i].user_id) {
@@ -123,10 +145,6 @@ scify.ConsultationReporterPageHandler.prototype = function(){
         createListOfComments = function(){
             var domElementOpenGovComments = document.getElementById("commentsOpenGov");
             var domElementDITComments = document.getElementById("commentsDIT");
-            //var commentListProperties = {
-            //    articleId          : articleId,
-            //    commentsCount      : numOfComments
-            //};
             window.OpenGovommentsPerArticleComponent = React.render(React.createElement(scify.commentList, null), domElementOpenGovComments);
             window.DITGovommentsPerArticleComponent = React.render(React.createElement(scify.commentList, null), domElementDITComments);
         },
