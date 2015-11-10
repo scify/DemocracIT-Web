caf73a372c6f9c634270c2c18c4bff1c037b37ab
paulisaris@gmail.com <paulisaris@gmail.com>
2015-11-10 14:07:34 +0200

    CORS application ptions initialized

--- app/utils/Filters.scala
+++ app/utils/Filters.scala
@@ -4,15 +4,12 @@ import javax.inject.Inject
 
 import play.api.http.HttpFilters
 import play.api.mvc.EssentialFilter
-import play.filters.csrf.CSRFFilter
-import play.filters.csrf.CSRFFilter
-import play.filters.headers.SecurityHeadersFilter
-import play.filters.headers.SecurityHeadersFilter
+import play.filters.cors.CORSFilter
 
 /**
  * Provides filters.
  */
 //class Filters @Inject() (csrfFilter: play.filters.csrf.CSRFFilter, securityHeadersFilter: SecurityHeadersFilter) extends HttpFilters {
-class Filters @Inject() (csrfFilter: play.filters.csrf.CSRFFilter) extends HttpFilters {
-  override def filters: Seq[EssentialFilter] = Seq(csrfFilter) //, securityHeadersFilter
+class Filters @Inject() (csrfFilter: play.filters.csrf.CSRFFilter, corsFilter: CORSFilter) extends HttpFilters {
+  override def filters: Seq[EssentialFilter] = Seq(csrfFilter, corsFilter) //, securityHeadersFilter
 }
