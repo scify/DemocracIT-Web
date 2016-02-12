568868b433f76575a5427c216da067db26bde309
paulisaris@gmail.com <paulisaris@gmail.com>
2016-02-12 14:41:08 +0200

    Fixed tutorial for consultations without comments on first article DW-596

--- public/js/consultation/scify-tutorialAnnotator.js
+++ public/js/consultation/scify-tutorialAnnotator.js
@@ -3,26 +3,28 @@ scify.TutorialAnnotator = function(consultationIsActive, assetsUrl) {
     this.assetsUrl = assetsUrl;
 }
 scify.TutorialAnnotator.prototype = (function(){
-
+    this.stepsForTutorial = [];
     var init = function(){
             var instance = this;
+            var elementToAddStep5;
+            var shouldAddStep5 = true;
             $(".title").find(".ann-icon").each(function(index){
                 //Added this block of code for creating the final step of the tutorial
                 if(index == 0) {
                     $(this).attr( "id", "step4" );
                     $(this).attr( "tutorial-id", "clicked");
                     //elementToAddStep5 is the first link to load comments
-                    var elementToAddStep5 = $(".article:first").find(".load:first");
+                    elementToAddStep5 = $(".article:first").find(".load:first");
                     //if the consultation is new (no comments yet), the element is empty
                     //so we should attach the step to the first annotation area
                     console.log(elementToAddStep5.length);
-                    if(elementToAddStep5.length == 0)
-                        elementToAddStep5 = $("body").find(".ann:first");
-                    $(elementToAddStep5).attr("id", "step5");
-
-
+                    //if the element is not found, it means that we do not have comments in the first article.
+                    if(elementToAddStep5.length == 0) {
+                        shouldAddStep5 = false
+                    } else {
+                        $(elementToAddStep5).attr("id", "step5");
+                    }
                 }
-                //
                 $(this).attr("title","κλικ εδώ για σχολιασμού όλου του άρθρου");
             });
 
@@ -30,14 +32,18 @@ scify.TutorialAnnotator.prototype = (function(){
             if(checkAndReadCookie()) {
                 $('[data-id="ann-0"]').find(".ann-icon").addClass("on");
                 $("[data-id='ann-1']").find(".ann-icon").addClass("on");
-                startIntro.call(instance);
+                startIntro.call(instance, shouldAddStep5);
             }
 
             $( "#tutorial" ).on( "click", function() {
                 $('[data-id="ann-0"]').find(".ann-icon").addClass("on");
                 $("[data-id='ann-1']").find(".ann-icon").addClass("on");
-                startIntro.call(instance);
+                startIntro.call(instance, shouldAddStep5);
             });
+
+        },
+        removeStepFromArr = function() {
+            console.log(this.stepsForTutorial);
         },
         checkAndReadCookie = function() {
             var tutorialCookie = getCookie("tutorialCookie");
@@ -66,10 +72,10 @@ scify.TutorialAnnotator.prototype = (function(){
             var expires = "expires="+d.toUTCString();
             document.cookie = cname + "=" + cvalue + "; " + expires;
         },
-        startIntro = function() {
+        startIntro = function(shouldAddStep5) {
             var intro = introJs();
-            var stepsForTutorial;
-            stepsForTutorial = [
+
+            this.stepsForTutorial = [
                 {
                     element: '#step3',
                     intro: '<div class="tutGif">Θέλετε να σχολιάσετε μία συγκεκριμένη φράση; Πατήστε το μολυβάκι δίπλα της. <img src="'+ this.assetsUrl + "/tutorialAnn.gif" +'" alt="Tutorial" height="300px" width="400px"></div>',
@@ -79,37 +85,41 @@ scify.TutorialAnnotator.prototype = (function(){
                     element: '#step4',
                     intro: "Θέλετε να σχολιάσετε ένα άρθρο συνολικά; Πατήστε το μολυβάκι δίπλα στον τίτλο του.",
                     position: 'right'
-                },
-                {
-                    element: '#step5',
-                    intro: 'Θέλετε να δείτε τα σχόλια άλλων πολιτών; Δείτε τα εδώ και εκφράστε τη γνώμη σας.',
-                    position: 'right'
                 }
             ];
             if(this.consultationIsActive) {
-                stepsForTutorial.splice(0, 0,{
+                this.stepsForTutorial.splice(0, 0,{
                     element: '#step1',
                     intro: "Εδώ εκφράζεστε! Δείτε το κείμενο της διαβούλευσης, τα σχόλια άλλων πολιτών και υποβάλετε τα δικά σας."
                 });
-                stepsForTutorial.splice(1,0,{
+                this.stepsForTutorial.splice(1,0,{
                     element: '#step2',
                     intro: "Μάθετε καλύτερα το θέμα πριν εκφραστείτε. Βρείτε εδώ το απαραίτητο σχετικό υλικό π.χ. την αιτιολογική έκθεση του νομοσχεδίου.",
                     position: 'bottom'
                 });
             } else {
-                stepsForTutorial.splice(0, 0,{
+                this.stepsForTutorial.splice(0, 0,{
                     element: '#step1',
                     intro: "Δείτε το κείμενο της διαβούλευσης και τα σχόλια των πολιτών."
                 });
-                stepsForTutorial.splice(1,0,{
+                this.stepsForTutorial.splice(1,0,{
                     element: '#step2',
                     intro: "Βρείτε εδώ το απαραίτητο σχετικό υλικό π.χ. την αιτιολογική έκθεση του νομοσχεδίου.",
                     position: 'bottom'
                 });
             }
+            if(shouldAddStep5) {
+                this.stepsForTutorial.push(
+                    {
+                        element: '#step5',
+                        intro: 'Θέλετε να δείτε τα σχόλια άλλων πολιτών; Δείτε τα εδώ και εκφράστε τη γνώμη σας.',
+                        position: 'right'
+                    }
+                );
+            }
 
             intro.setOptions({
-                steps: stepsForTutorial,
+                steps: this.stepsForTutorial,
                 exitOnEsc: true,
                 keyboardNavigation: true,
                 scrollToElement: true
